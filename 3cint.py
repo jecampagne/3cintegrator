16dcc3b2266e585f14b5510c33d8cbc44a0a78e4
Guy Barrand <guy.barrand@gmail.com>
2020-12-07 16:28:50 +0100

    update

--- 3cint.py
+++ 3cint.py
@@ -1,9 +1,11 @@
 
 import Angpow
 
-class FuncType0:
+class FuncType0(Angpow.ClassFunc1D):
   dummy = 2
-#  def __init__(self):
+  def __init__(self):
+    dummy = 2
+    
 #public Angpow.ClassFunc1D
 #  inline virtual r_8 operator()(r_8 x) const {
 #    return x * (x-1.)*(x-1.);
@@ -16,10 +18,11 @@ class FuncType0:
 def test0():
   f0 = FuncType0()
   
-  iOrd0 = 2;
+  iOrd0 = 2
   
-#  std::vector<CheFunc*> farr;
-#  farr.push_back(new CheFunc(f0, iOrd0));
+  farr = Angpow.std_vector_CheFunc()
+  #che_func_0 = Angpow.CheFunc(f0,iOrd0)
+  #farr.push_back(Angpow.CheFunc(f0,iOrd0))
   
 #  CheAlgo cheAlgo(farr);
   print('End test0......');
