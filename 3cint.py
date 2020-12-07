f6979d4f8ebd0c48815efc7135e1688495c9f068
Guy Barrand <guy.barrand@gmail.com>
2020-12-07 18:28:31 +0100

    update

--- 3cint.py
+++ 3cint.py
@@ -11,15 +11,12 @@ import math
 class FuncType1(Angpow.ClassFunc1D):
   m_ell = 0
   m_R = 0
-  #def __init__(self):
-  #  self.m_ell = 0
-  #  self.m_R = 0
-  def __init__(self,a_ell,a_R):
-    #Angpow.ClassFunc1D()
-    self.m_ell = a_ell
-    self.m_R = a_R
+  #def __init__(self,a_ell,a_R):
+  #  #Angpow.ClassFunc1D()
+  #  self.m_ell = a_ell
+  #  self.m_R = a_R
   def get_value(self,a_x):
-    return math.cos(a_x*self.m_R - self.m_ell*math.pi*0.5 - math.pi*0.25);
+    return math.cos(a_x*self.m_R - self.m_ell*math.pi*0.5 - math.pi*0.25)
 
 def test0():
   ell = 20
@@ -28,8 +25,16 @@ def test0():
   chebyshev_order_1 = 8
   chebyshev_order_2 =  chebyshev_order_1
 
-  f1 = FuncType1(ell,R1);
-  f2 = FuncType1(ell,R2);
+  #f1 = FuncType1(ell,R1)
+  f1 = FuncType1()
+  f1.m_ell = ell
+  f1.m_R = R1
+  
+  #f2 = FuncType1(ell,R2)
+  f2 = FuncType1()
+  f2.m_ell = ell
+  f2.m_R = R2
+
   f0 = FuncType0()
   
   #print(f1.get_value(1))
@@ -45,6 +50,6 @@ def test0():
   farr.push_back(Angpow.CheFunc(f0, iOrd0))
   
 #  CheAlgo cheAlgo(farr);
-  print('End test0......');
+  print('End test0......')
 
 test0()
