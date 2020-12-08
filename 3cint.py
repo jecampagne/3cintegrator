74741f8bb29d139ab23a2b4bfae5b3be1192151d
Guy Barrand <guy.barrand@gmail.com>
2020-12-08 12:04:00 +0100

    update

--- 3cint.py
+++ 3cint.py
@@ -33,7 +33,8 @@ def test0():
   dK = kMax-kMin
   for i in range(0,n_sub_intervals+1):
     klp[i] = kMin + dK * i/n_sub_intervals
-  print("ell=%d, Nintervales=%d\n", ell,n_sub_intervals)
+    print "klp[",i,"]",klp[i]
+  print "ell=",ell," , Nintervales=",n_sub_intervals
 
   #f1 = FuncType1(ell,R1)
   f1 = FuncType1()
@@ -54,18 +55,18 @@ def test0():
   iOrd2 = chebyshev_order_2
   
   farr = Angpow.std_vector_CheFunc()
-  print("debug : add CheFunc : 000")
+  print "debug : add CheFunc : 000"
   che_fun_0 = Angpow.CheFunc(f1, iOrd1)
   farr.push_back(che_fun_0)
 
-  print("debug : add CheFunc : 001")
+  print "debug : add CheFunc : 001"
   che_fun_1 = Angpow.CheFunc(f2, iOrd2)
   farr.push_back(che_fun_1)
 
-  print("debug : add CheFunc : 002")
+  print "debug : add CheFunc : 002"
   che_fun_2 = Angpow.CheFunc(f0, iOrd0)
   farr.push_back(che_fun_2)
-  print("debug : add CheFunc : 003")
+  print "debug : add CheFunc : 003"
   
   # Initialisation of the Clenshow-Curtis quadrature
   cheAlgo = Angpow.CheAlgo(farr)
@@ -79,23 +80,27 @@ def test0():
     uppBound = klp[p]
     
     if lowBound > uppBound:
-      print('KIntegrator::Compute uppBound < lowBound Fatal')
+      print 'KIntegrator::Compute uppBound < lowBound Fatal'
       return
     
     # Loop on each function to compute their  Foward Chebyshev coefficents
-    print('debug : test0 : 000')
+    print 'debug : test0 : 000-0 ',farr.size()
     for i in range(0,farr.size()):
+      print 'debug : test0 : 000-1 ',i
       farr[i].ChebyshevTransform(lowBound, uppBound)
-    print('debug : test0 : 001')
+    print 'debug : test0 : 001'
 
     # Compute the sampling of all the functions in the final space dimension
     cheAlgo.InverseChebyshevTransform()
 
     # Compute the integral thanks to CC quadrature and the function sampling 
     integral += (uppBound - lowBound) * cheAlgo.ComputeIntegralUnscaled()
+    #i_unscaled = cheAlgo.ComputeIntegralUnscaled()
+    #print i_unscaled
+    #integral += i_unscaled
     
-  print("Approx. Integ = %d",integral)
+  print "Approx. Integ = ",integral
 
-  print('End test0......')
+  print 'End test0......'
 
 test0()
