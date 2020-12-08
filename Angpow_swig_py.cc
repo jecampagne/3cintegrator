a5daf4cc650d3659ce3dd69ad2c36f201057ee2e
Guy Barrand <guy.barrand@gmail.com>
2020-12-08 15:01:27 +0100

    update

--- Angpow_swig_py.cc
+++ Angpow_swig_py.cc
@@ -4,14 +4,17 @@
 #include "inc/3CInt/3cint_chealgo.h"
 
 /*
+*/
 namespace Angpow {
 class Py_ClassFunc1D : public ClassFunc1D {
   typedef ClassFunc1D parent;
 public:
-  virtual double get_value(double x) const = 0;
+  virtual double get_value(double x) const {
+    printf("debug : Py_ClassFunc1D::get_value %g\n",x);
+    return 0;
+  }
   virtual double operator()(double x) const {return get_value(x);}
 };
 }
-*/
 
 #include "Angpow_swig_py.icc"
